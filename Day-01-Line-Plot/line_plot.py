4b099cc3c5be4a24e939d9a5b1cb116d56954128
SIVA NAIK MUDAVATHU <148525397+SIVANAIK05@users.noreply.github.com>
2026-08-03 21:34:18 +0530

    Update line_plot.py

--- Day-01-Line-Plot/line_plot.py
+++ Day-01-Line-Plot/line_plot.py
@@ -1,14 +1,15 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+ 
 
-# Load the dataset
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv(
-    "../Datasets/Toyota.csv",
+    r'"../datasets/Toyota.csv"',
     index_col=0,
     na_values=["??", "???"]
 )
-
 # Remove missing values
 df.dropna(inplace=True)
 
