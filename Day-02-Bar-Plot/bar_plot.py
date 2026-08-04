ca4132c35e03465b9f3d70279276bf0047ab2c37
SIVA NAIK MUDAVATHU <148525397+SIVANAIK05@users.noreply.github.com>
2026-08-04 18:28:58 +0530

    Update bar_plot.py

--- Day-02-Bar-Plot/bar_plot.py
+++ Day-02-Bar-Plot/bar_plot.py
@@ -1,1 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Load the dataset
+df = pd.read_csv(
+    "../Datasets/Toyota.csv",
+    index_col=0,
+    na_values=["??", "???"]
+)
+
+# Remove missing values
+df.dropna(inplace=True)
+
+# Calculate average price for each age
+age_price = df.groupby('Age')['Price'].mean().sort_index()
+
+# Create Bar Plot
+age_price.plot(kind='bar')
+
+# Add title and labels
+plt.title('Age Price vs Age')
+plt.xlabel('Age')
+plt.ylabel('Price')
+
+# Display the graph
+plt.show()
