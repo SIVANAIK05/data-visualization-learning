7939a72fe6fdac4af416e54d2212a9fad5d6ae4c
SIVA NAIK MUDAVATHU <148525397+SIVANAIK05@users.noreply.github.com>
2026-08-05 11:37:08 +0530

    Update scatter_plot.py

--- Day-03-Scatter-Plot/scatter_plot.py
+++ Day-03-Scatter-Plot/scatter_plot.py
@@ -1,1 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Load the dataset
+df = pd.read_csv(
+    "../Datasets/Toyota.csv",
+    index_col=0,
+    na_values=["??", "???"]
+)
+
+# Remove missing values
+df.dropna(inplace=True)
+
+# Create Scatter Plot
+plt.figure(figsize=(8,5))
+plt.scatter(df['Age'], df['Price'], color='black')
+
+# Add title and labels
+plt.title('Age vs Price')
+plt.xlabel('Age')
+plt.ylabel('Price')
+
+# Display the graph
+plt.show()
