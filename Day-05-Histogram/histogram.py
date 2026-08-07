610e21a06b7ec45d06622d8dc8188672642f0246
SIVA NAIK MUDAVATHU <sivanaikmudavath05@gmail.com>
2026-08-07 15:09:07 +0530

    Update histogram.py

--- Day-05-Histogram/histogram.py
+++ Day-05-Histogram/histogram.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Load the dataset
+df = pd.read_csv(
+    "../Datasets/Toyota.csv",
+    index_col=0,
+    na_values=["??", "???"]
+)
+
+# Remove missing values
+df.dropna(inplace=True)
+
+plt.figure(figsize=(10,10))
+plt.hist(df['KM'], bins=20, color='blue', edgecolor='black' )
+plt.title('Histogram of KM')
+plt.xlabel('KM')
+plt.ylabel('Frequency')
+plt.show()
