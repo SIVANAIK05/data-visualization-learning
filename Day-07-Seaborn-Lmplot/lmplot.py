8f96f3ea4043f93e43438465ce47e4ee7ba99767
SIVA NAIK MUDAVATHU <sivanaikmudavath05@gmail.com>
2026-08-09 08:43:57 +0530

    Update lmplot.py

--- Day-07-Seaborn-Lmplot/lmplot.py
+++ Day-07-Seaborn-Lmplot/lmplot.py
@@ -1,1 +1,34 @@
+import pandas as pd
 
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv(
+    r' "../datasets/Toyota.csv"',
+    index_col=0,
+    na_values=["??", "???"]
+)
+# Remove missing values
+df.dropna(inplace=True)
+
+sns.lmplot(x='Age', y='Price', hue='FuelType', data=df,fit_reg=True, palette='Set1')
+plt.title('Age vs Price')
+
+plt.show()
+
+
+
+#========Lmplot with markers+++++++++++++++++
+
+sns.lmplot(
+    x='Age',
+    y='Price',
+    hue='FuelType',
+    data=df,fit_reg=False,
+    markers=["o","X","*"],
+    palette='tab10'
+)
+plt.title('Age vs Price')
+plt.show()
