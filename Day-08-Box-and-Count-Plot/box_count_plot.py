97720fd5f097dee1d99a5118776973fe42b08e9f
SIVA NAIK MUDAVATHU <sivanaikmudavath05@gmail.com>
2026-08-10 08:37:52 +0530

    Update box_count_plot.py

--- Day-08-Box-and-Count-Plot/box_count_plot.py
+++ Day-08-Box-and-Count-Plot/box_count_plot.py
@@ -1,1 +1,29 @@
 
+import pandas as pd
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv(
+    r' "../datasets/Toyota.csv"',
+    index_col=0,
+    na_values=["??", "???"]
+)
+# Remove missing values
+df.dropna(inplace=True)
+#==========================COUNT PLOT==================
+
+#sns.countplot(x='FuelType',  data=df)
+sns.countplot(x='FuelType', hue='Automatic' ,data=df)
+plt.title('Age vs Price')
+plt.show()
+
+
+#======================BOX PLOT======================
+
+sns.boxplot(x='Price',  hue='Automatic',data=df)
+plt.title('Age vs Price')
+plt.show()
+
