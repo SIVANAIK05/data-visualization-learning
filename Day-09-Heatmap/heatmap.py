b8608d2e65a288a8bb22bbc16783a378dbc614d4
SIVA NAIK MUDAVATHU <sivanaikmudavath05@gmail.com>
2026-08-11 08:34:07 +0530

    Update heatmap.py

--- Day-09-Heatmap/heatmap.py
+++ Day-09-Heatmap/heatmap.py
@@ -1,1 +1,28 @@
 
+import pandas as pd
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv(
+    r' "../datasets/Toyota.csv"',
+    index_col=0,
+    na_values=["??", "???"]
+)
+# Remove missing values
+df.dropna(inplace=True)
+
+corr_matrix = df.select_dtypes(include=[np.number]).corr()
+
+plt.figure(figsize=(10, 8))
+
+sns.heatmap(
+    corr_matrix,
+    annot=True,
+    cmap="coolwarm"
+)
+
+plt.title("Correlation Matrix")
+plt.show()
